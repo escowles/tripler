0ffeb88f1be19e5d913a963802d2ce70eeab2230
Esmé Cowles <escowles@ticklefish.org>
2021-05-10 17:03:06 -0400

    Adding support for references to Objs or Subjects as statement objects

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,6 +1,37 @@
 class Statement < ApplicationRecord
   belongs_to :subject
   belongs_to :predicate
+  belongs_to :obj, optional: true
+  belongs_to :resource_object, class_name: "Subject", optional: true
   validates_associated :subject, :predicate
-  validates :literal, presence: true
+  validate :only_one_object
+
+  def statement_object
+    return literal if literal
+    return resource_object if resource_object
+    return obj
+  end
+
+  def statement_object=(literal_or_resource)
+    if literal_or_resource.kind_of?(Subject)
+      self.obj = nil
+      self.literal = nil
+      self.resource_object = literal_or_resource
+    elsif literal_or_resource.kind_of?(Obj)
+      self.literal = nil
+      self.resource_object = nil
+      self.obj = literal_or_resource
+    else
+      self.obj = nil
+      self.resource_object = nil
+      self.literal = literal_or_resource
+    end
+  end
+
+  def only_one_object
+    n = [literal.present?, obj.present?, resource_object.present?].count(true)
+    unless n == 1
+      errors.add :statement_object, "Exactly one of :literal, :obj, or :resource_object must be provided"
+    end
+  end
 end
